58d37b021edaa4f2d2be2c20ecbe59e27a2ba596
ahmed65S <83534532+ahmed65S@users.noreply.github.com>
2022-05-26 12:00:06 +0200

    Add files via upload

---
+++ src/beans/client.java
@@ -0,0 +1,25 @@
+package beans;
+
+public class ClientBean {
+
+	public void setNom(String nom) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setPrenom(String nom) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setAdresse(String nom) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setTelephon(String nom) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setEmail(String nom) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/controllers/authentificationservlet.java
@@ -0,0 +1,48 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class authentificationservlet
+ */
+@WebServlet("/authentificationservlet")
+public class authentificationservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public authentificationservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nom = request.getParameter("T1");
+		 String prenom = request.getParameter("T2");
+
+		HttpSession session=request.getSession();
+		if (session.getAttribute("authbean")==null){
+			
+		}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
